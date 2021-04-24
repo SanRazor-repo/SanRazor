ddd2ab9e795663d39e40ddb1eccce6bcbac7cf37
SanRazor-repo <sanrazor2020@gmail.com>
2021-04-23 18:23:43 -0700

    add some scripts for cve

--- test/test_mp3gain_asan.sh
+++ test/test_mp3gain_asan.sh
@@ -16,10 +16,11 @@ make CC=SanRazor-clang CXX=SanRazor-clang++ CFLAGS="-Wall -Winline -g -O3 -fsani
 
 cp -r ../Profiling ./
 cd Profiling
+sudo chmod -R 777 *
 ./profiling.sh
 cd ../
 
-SanRazor-clang -SR-opt -san-level=L0 -use-asap=1.0
+SanRazor-clang -SR-opt -san-level=L$1 -use-asap=1.0
 make clean
 make CC=SanRazor-clang CXX=SanRazor-clang++ CFLAGS="-Wall -Winline -g -O3 -fsanitize=address"  LIBS="-lm -fsanitize=address" -j 12
 
